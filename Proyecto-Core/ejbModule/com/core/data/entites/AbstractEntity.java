dc162a4e91c8717f1d4e84c9573d8c409dd67fb4
napache <napache02@gmail.com>
2014-10-30 22:50:17 -0200

    FIX faltaba esta clase

---
+++ Proyecto-Core/ejbModule/com/core/data/entites/AbstractEntity.java
@@ -0,0 +1,12 @@
+package com.core.data.entites;
+
+import java.io.Serializable;
+
+public class AbstractEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -118841918992845435L;
+	
+}
