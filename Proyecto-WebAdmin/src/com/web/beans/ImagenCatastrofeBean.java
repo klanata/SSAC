464d2b78ea6f2fda0c945e23bb7059255c7c6899
CeciliaCamp <crcamps@gmail.com>
2014-11-29 09:58:04 -0200

    x

---
+++ Proyecto-WebAdmin/src/com/web/beans/ImagenCatastrofeBean.java
@@ -0,0 +1,63 @@
+package com.web.beans;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.core.data.entites.Catastrofe;
+
+
+
+@ManagedBean(name="imagenCatastrofeBean")
+@SessionScoped
+public class ImagenCatastrofeBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String path;	
+	private Catastrofe catastrofe;
+		
+	
+	//	------------------ Constructors  --------------------------------
+	
+	
+	public ImagenCatastrofeBean() {	
+	}	
+	public ImagenCatastrofeBean(Long id, String path, Catastrofe catastrofe) {
+		super();
+		this.id = id;
+		this.path = path;
+		this.setCatastrofe(catastrofe);
+		
+	}
+	
+	
+	//	------------------ Getter and setter methods ---------------------
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public Catastrofe getCatastrofe() {
+		return catastrofe;
+	}
+	public void setCatastrofe(Catastrofe catastrofe) {
+		this.catastrofe = catastrofe;
+	}
+	
+	
+		
+
+}
