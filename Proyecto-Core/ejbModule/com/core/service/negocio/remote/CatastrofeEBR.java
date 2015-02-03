7b154687fc8394876f3d8be6d4e756146a78b211
CeciliaCamp <crcamps@gmail.com>
2015-02-02 23:40:48 -0200

    Nueva función de catastrofe

--- Proyecto-Core/ejbModule/com/core/service/negocio/remote/CatastrofeEBR.java
+++ Proyecto-Core/ejbModule/com/core/service/negocio/remote/CatastrofeEBR.java
@@ -62,4 +62,6 @@ public interface CatastrofeEBR {
 	public void asignarFiltroServicioALaCatastrofe(Long idCatastrofe, Long idFiltroServicio) throws Exception;
 	
 	public void eliminarFiltroServicioDeCatastrofe(Long idFiltroServicio) throws Exception;
+	
+	public List<String> listarFiltroDeCatastrofe(long idCatastrofe, String fuente) throws Exception;
 }
